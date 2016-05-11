04e02fb1b9820979957d29b7be29d0cdc804cb95
Thomas-Ko <tomkolodzi@gmail.com>
2016-05-11 11:39:29 -0400

    Set model.currentWeather data from retrieved object from ajax call

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,8 @@ var model = {
 			imgURL: null,
 		}
 	}
-};
+
+};  //end model
 
 /*====================
 	CONTROLLER
@@ -42,12 +43,47 @@ controller = {
 
 			url: "http://api.wunderground.com/api/32d6346cb727aad9/forecast/geolookup/conditions/q/autoip.json?",
 
+			dataType: "jsonp",
+
 			success: function( response ) {
 				console.log(response);
+
+	        	// var obj = JSON.parse(response);
+	        	/*I do not need to run the above code because jQuery automatically parses the datatype if it is jsonp.
+	        	Running the above code would return an error.*/
+
+	        	controller.setCurrentWeather(response.current_observation);
 			},
 		});
+	},
 
+	setCurrentWeather: function(data) {
 		
+		// var weather = obj.current_observation;
+
+		model.currentWeather= {
+			location: data.display_location.full,
+			temp: {
+				c: data.temp_c,
+				f: data.temp_f,
+			},
+			feelsLike: {
+				c: data.feelslike_c,
+				f: data.feelslike_f,
+			},
+			weather: data.weather,
+			wind: data.wind_mph,
+			precipitation: data.precip_today_in,
+			humidity: data.relative_humidity,
+			icon: {
+				name: data.icon,
+				imgURL: data.icon_url,
+			}
+		};
+	},
+
+	getCurrentWeather: function() {
+		return model.currentWeather;
 	}
 
 }; //end controller
